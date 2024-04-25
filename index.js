2c075e31be2599dce1eda620007431a1d9286ccc
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-04-25 16:33:28 +0530

    Sample db client

---
+++ index.js
@@ -0,0 +1,21 @@
+const {MongoClient} = require("mongodb");
+const url = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(url);
+const dbName = "myDatabase";
+//console.log(client);
+
+async function main(){
+    /*client.connect() is an asynchronous operation that connects
+    to the MongoDB server. By using await, the execution of main()
+    is paused until the connection is established. This ensures
+    that subsequent operations, which rely on the connection,
+    won't execute until it's ready. */
+    await client.connect();
+    console.log("Connected Succesfully to mongodb server");
+    const db = client.db(dbName);
+    const collection = db.collection('users');
+    const findResult = await collection.find().toArray(); //find() returns a cursor, toArray() is called to convert the cursor to an array of documents
+    console.log(findResult);
+};
+
+main();
